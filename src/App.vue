1e9477a5f5e84c3e65989722d612628c42c8daf4
wangyueweb <120400787@qq.com>
2017-10-26 18:00:21 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <!-- 头部组件 -->
+    <m-header></m-header>
+    <!-- 导航组件 -->
+    <tab></tab>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
+
+  export default {
+    components: {
+      MHeader,
+      Tab
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
